7f1d219ad35c8f5c211f02592e3e4ecea3cbe05e
saljaoui <soufian2022ff@gmail.com>
2024-09-01 19:40:06 +0100

    s

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ func main() {
 	http.HandleFunc("/geoMap", Groupie_tracker.GeoMap)
 	http.HandleFunc("/filters/", Groupie_tracker.Filters)
 	http.HandleFunc("/styles/", Groupie_tracker.HandleStyle)
-	fmt.Printf("http://localhost%s\n", port)
+	fmt.Printf("http://localhost%s", port)
+	fmt.Println()
 	log.Fatal(http.ListenAndServe(port, nil))
 }
